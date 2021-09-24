3915433e22e9ede2759d263ebe9f6aefbe4d8c5e
harshmangat <harshmangat0007@gmail.com>
2021-09-24 12:47:44 +0530

    added apis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "./components/PostForm";
 import PostForm from "./components/PostForm";
 import "./components/PostIt.css";
 import PostCard from "./components/PostCard";
+import useHttp from "./hooks/use-http";
 
-const postObj = [
-  {
-    id: "e1",
-    title: "Title will be shown here",
-    text: " Write some quote!",
-    url: "https://images.unsplash.com/photo-1628626110189-f963f1ec6755?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80",
-    category: "Books",
-  },
-];
+// const postObj = [
+//   {
+//     id: "e1",
+//     title: "Title will be shown here",
+//     text: " Write some quote!",
+//     url: "https://images.unsplash.com/photo-1628626110189-f963f1ec6755?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=749&q=80",
+//     category: "Books",
+//   },
+// ];
 
 function App() {
-  const [data, setData] = useState(postObj);
+  const [data, setData] = useState([]);
+  // const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (data) => {
+      const loadedTasks = data.results.map((data) => ({
+        id: data.objectId,
+        text: data.description,
+        title: data.title,
+        url: data.image,
+        category: data.category
+      }));
+      setData(loadedTasks);
+    };
+    fetchTasks(
+      {
+        method: "get",
+        url: "https://peerup-web-dev-srv.herokuapp.com/parse/classes/PostIt",
+        headers: {
+          "X-Parse-Application-Id": "MVV6avFp",
+          "Content-Type": "application/json"
+        }
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const addNewPostHandler = (postData) => {
     setData((prevData) => {
@@ -25,9 +53,11 @@ function App() {
   };
   return (
     <div>
-      <PostForm newPost={addNewPostHandler}/>
+      <PostForm newPost={addNewPostHandler} />
       {data.map((post) => (
         <PostCard
+          isLoading={isLoading}
+          error={error}
           key={post.id}
           title={post.title}
           url={post.url}
